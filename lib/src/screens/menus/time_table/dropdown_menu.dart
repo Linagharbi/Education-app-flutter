a7f0e6c1af7d6e49d150f2fe1da132f20a37c3cb
UserSina <benna.anis2@gmail.com>
2020-08-17 11:30:00 +0100

    Adjust time table dropdown
    -Avoiding overflow on long names

--- lib/src/screens/menus/time_table/dropdown_menu.dart
+++ lib/src/screens/menus/time_table/dropdown_menu.dart
@@ -17,52 +17,55 @@ class _DropDownMenuState extends State<DropDownMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      hint: Text(
-        "Select child",
-        style: TextStyle(
-          color: Color(0xFF8e7daf),
+    return FittedBox(
+      fit: BoxFit.contain,
+      child: DropdownButton<String>(
+        hint: Text(
+          "Select child",
+          style: TextStyle(
+            color: Color(0xFF8e7daf),
+          ),
         ),
-      ),
-      icon: Icon(
-        // Add this
-        Icons.arrow_drop_down, // Add this
-        color: Colors.white, // Add this
-      ),
-      value: selectedChild,
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 15,
-      ),
-      selectedItemBuilder: (selectedChild != null)
-          ? (BuildContext context) {
-              return children.map<Widget>(
-                (String value) {
-                  return Center(
-                    child: Text(
-                      selectedChild,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+        icon: Icon(
+          // Add this
+          Icons.arrow_drop_down, // Add this
+          color: Colors.white, // Add this
+        ),
+        value: selectedChild,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 15,
+        ),
+        selectedItemBuilder: (selectedChild != null)
+            ? (BuildContext context) {
+                return children.map<Widget>(
+                  (String value) {
+                    return Center(
+                      child: Text(
+                        selectedChild,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  );
-                },
-              ).toList();
-            }
-          : null,
-      items: children.map((item) {
-        return DropdownMenuItem(
-          value: item,
-          child: Text(item),
-        );
-      }).toList(),
-      onChanged: (value) {
-        setState(() {
-          selectedChild = value;
-        });
-      },
+                    );
+                  },
+                ).toList();
+              }
+            : null,
+        items: children.map((item) {
+          return DropdownMenuItem(
+            value: item,
+            child: Text(item),
+          );
+        }).toList(),
+        onChanged: (value) {
+          setState(() {
+            selectedChild = value;
+          });
+        },
+      ),
     );
   }
 }
