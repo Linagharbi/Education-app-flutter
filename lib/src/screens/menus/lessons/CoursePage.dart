f62d7f1c95e592b18e94eb692993d67369315c80
Lina Gharbi <linagharbi8@gmail.com>
2020-08-21 10:06:06 +0100

    Update CoursePage.dart

--- lib/src/screens/menus/lessons/CoursePage.dart
+++ lib/src/screens/menus/lessons/CoursePage.dart
@@ -180,7 +180,24 @@ class _CoursePageState extends State<CoursePage> {
                   style: TextStyle(fontSize: 16, color: Colors.grey),
                 )
               ],
-            )
+            ),
+            SizedBox(
+              width: 100,
+            ),
+            Container(
+              width: 25,
+              height: 25,
+              // decoration: BoxDecoration(
+              //   color: Color(0xff2657ce),
+              //   borderRadius: BorderRadius.all(Radius.circular(17)),
+              // ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.picture_as_pdf,
+                  color: Color(0xff2657ce),
+                ),
+              ),
+            ),
           ],
         ),
         Center(
