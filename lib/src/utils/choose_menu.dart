541c375465ef447bd51d786fb0279a1eb7635667
UserSina <benna.anis2@gmail.com>
2020-08-29 16:51:19 +0100

    Update dependencies

--- lib/src/utils/choose_menu.dart
+++ lib/src/utils/choose_menu.dart
@@ -1,5 +1,4 @@
 import 'package:education_app/src/screens/menus/events/events.dart';
-import 'package:education_app/src/screens/menus/events/events.dart';
 import 'package:education_app/src/screens/menus/grades/grades.dart';
 import 'package:education_app/src/screens/menus/lessons/lessons.dart';
 import 'package:education_app/src/screens/menus/discipline/discipline.dart';
